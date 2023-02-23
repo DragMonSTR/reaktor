a1812e3a4b672f0ef3b89d9a50319d18c9f6c178
Vladyslav <80710398+DragMonSTR@users.noreply.github.com>
2023-02-23 09:05:13 +0100

    Set try_to_update_drive_delay to 5s

--- apps/monitor/constants.py
+++ apps/monitor/constants.py
@@ -3,7 +3,7 @@ class Constants:
     CASH_DIR = 'cash'
     CASH_FILE_PATH = 'cash/data.xlsx'
     PUBLIC_FILE_PATH = '../../data/data.xlsx'
-    TRY_TO_UPDATE_DRIVE_DELAY = 10
+    TRY_TO_UPDATE_DRIVE_DELAY = 5
     CLOUD_DATA_DIR = 'reaktorBackup'
     CLOUD_TOKEN_PATH = 'cloud/token.json'
     CLOUD_CREDENTIALS_PATH = 'cloud/credentials.json'
