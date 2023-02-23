b9a0484f17fc182f730842720f8f7f84b5add761
Vladyslav <80710398+DragMonSTR@users.noreply.github.com>
2023-02-23 09:03:48 +0100

    Add all constants to constants.py

--- apps/monitor/constants.py
+++ apps/monitor/constants.py
@@ -1,2 +1,9 @@
 class Constants:
     CONFIGURATION_FILE_PATH = '../../data/configuration.txt'
+    CASH_DIR = 'cash'
+    CASH_FILE_PATH = 'cash/data.xlsx'
+    PUBLIC_FILE_PATH = '../../data/data.xlsx'
+    TRY_TO_UPDATE_DRIVE_DELAY = 10
+    CLOUD_DATA_DIR = 'reaktorBackup'
+    CLOUD_TOKEN_PATH = 'cloud/token.json'
+    CLOUD_CREDENTIALS_PATH = 'cloud/credentials.json'
